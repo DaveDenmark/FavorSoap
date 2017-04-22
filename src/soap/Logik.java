ec885e5d19a17ba9e29d74bb3e374b6d6bf76f07
TheDave <davidjkristensen@gmail.com>
2017-04-22 22:02:37 +0200

    Setting up HTTP
    Http

--- src/soap/Logik.java
+++ src/soap/Logik.java
@@ -2,11 +2,21 @@ package soap;
 
 import brugerauth.Bruger;
 import brugerauth.Brugeradmin;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import javax.jws.WebService;
 import javax.ws.rs.core.Cookie;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import java.net.URL;
+import sun.net.www.http.HttpClient;
+
 
 @WebService(endpointInterface = "soap.LogikI")
 public class Logik implements LogikI {
@@ -27,10 +37,31 @@ public class Logik implements LogikI {
         adgangsKode = adgangskode;
         return new Cookie(bruger,adgangskode);
     }
+  public String makeServiceCall(String reqUrl) {
+            String response = null;
+            try {
+                URL url = new URL(reqUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                // read the response
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+                response = convertStreamToString(in);
+            } catch (MalformedURLException e) {
+                
+            } catch (ProtocolException e) {
+                
+            } catch (IOException e) {
+                
+            } catch (Exception e) {
+                
+            }
+            return response;
+        }
 
-    public boolean getOrders(Cookie cookie) {
-        return false;
-    }
+    public String getOrders(Cookie cookie) {
+       String get = makeServiceCall("https://favordrop.firebaseio.com/orders.json");
+        return get;
+    }   
 
     public boolean getClients(Cookie cookie) {
         return false;
@@ -39,4 +70,25 @@ public class Logik implements LogikI {
     public boolean getPartners(Cookie cookie) {
         return false;
     }
+
+    private String convertStreamToString(InputStream in) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append('\n');
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return sb.toString();
+        }
 }
--- src/soap/LogikI.java
+++ src/soap/LogikI.java
@@ -7,10 +7,11 @@ import javax.ws.rs.core.Cookie;
 /**
  * Created by Rasmus on 12-04-2017.
  */
+
 @WebService 
 public interface LogikI {
     @WebMethod Cookie login(String bruger, String adgangskode) throws Exception;
     @WebMethod boolean getClients(Cookie cookie);
-    @WebMethod boolean getOrders(Cookie cookie);
+    @WebMethod String getOrders(Cookie cookie);
     @WebMethod boolean getPartners(Cookie cookie);
 }
