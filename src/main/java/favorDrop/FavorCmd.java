f5336c53b8e380f13473c4e9632448591897e0e1
Henrik <Carzita@users.noreply.github.com>
2017-05-14 12:39:37 +0200

    added missing methods from logic to cmd class

--- src/main/java/favorDrop/FavorCmd.java
+++ src/main/java/favorDrop/FavorCmd.java
@@ -11,13 +11,11 @@ import java.util.Scanner;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
-
 /**
  *
  * @author Henrik
  */
 public class FavorCmd {
-    
     
     public static void main(String[] args) throws MalformedURLException, Exception{
         run();
@@ -55,9 +53,11 @@ public class FavorCmd {
             System.out.println("\n"+"Tryk 1 for at se antal bruger i DB");
             System.out.println("Tryk 2 for at se antal partnere i DB");
             System.out.println("Tryk 3 for at se antal færdige ordre");
-            System.out.println("Tryk 4 for at slette en ny ordre");
-            System.out.println("Tryk 5 for at slette en igangværende ordre");
-            System.out.println("Tryk 6 for at afslutte");
+            System.out.println("Tryk 4 for at hente en liste af nye ordre ID'er");
+            System.out.println("Tryk 5 for at slette en ny ordre");
+            System.out.println("Tryk 6 for at hente en liste af igangværende ordre ID'er");
+            System.out.println("Tryk 7 for at slette en igangværende ordre");
+            System.out.println("Tryk 8 for at afslutte");
             if (scan.hasNextInt()){
                 number=scan.nextInt();
                 scan.nextLine();
@@ -79,18 +79,28 @@ public class FavorCmd {
                     System.out.println("\n"+"Antal gennemførte ordre i DB: " + returned);
                     break;
                 case 4:
-                    System.out.println("Indtast OrdreID som du vil slette");
+                    System.out.println("\n"+"Nye ordrer:");
+                    returned = logik.getOrdersNew(login);
+                    System.out.println(returned);
+                    break;
+                case 5:
+                    System.out.println("\n"+"Indtast new OrdreID som du vil slette");
                     OID = scan.nextLine();
                     returned = logik.deleteOrderNew(login, OID);
                     System.out.println(returned);
                     break;
-                case 5:
-                    System.out.println("Indtast OrdreID som du vil slette");
+                case 6:
+                    System.out.println("\n"+"Igangværende ordrer:");
+                    returned = logik.getOrdersInService(login);
+                    System.out.println(returned);
+                    break;
+                case 7:
+                    System.out.println("\n"+"Indtast in service OrdreID som du vil slette");
                     OID = scan.nextLine();
                     returned = logik.deleteOrderInService(login, OID);
                     System.out.println(returned);
                     break;
-                case 6:
+                case 8:
                     scan.close();
                     break whileLoop;
                 default: System.out.println("\n"+"Ugyldigt tal, prøv igen");
